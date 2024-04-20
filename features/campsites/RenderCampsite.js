890833ff78d17755da99e858065d961234b1c36f
Hannah <hannahfranco98@gmail.com>
2024-04-20 08:27:26 -0500

    Using Redux

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
+import { BaseUrl } from '../../shared/baseUrl';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
     if (campsite) {
         return (
             <Card containerStyle={styles.cardContainer}>
-                <Card.Image source={campsite.image}>
+                <Card.Image source={{ uri: baseUrl + campsite.Image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { COMMENTS } from '../shared/comments';
+import { useSelector } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
+    const comments = useSelector((state) => state.comments);
 
-    const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite] = useState(false);
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -22,7 +23,7 @@ const CampsiteInfoScreen = ({ route }) => {
 
     return (
         <FlatList
-            data={comments.filter(
+            data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
             renderItem={renderCommentItem}
@@ -33,7 +34,11 @@ const CampsiteInfoScreen = ({ route }) => {
             }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite campsite={campsite} />
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorite}
+                        markFavorite={() => setFavorite(true)}
+                    />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
