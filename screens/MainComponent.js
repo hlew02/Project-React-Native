a23b2c0b20841ae155b0eb6ebe920176fd96335e
Hannah <hannahfranco98@gmail.com>
2024-05-04 11:10:15 -0500

    Week 4 Workshop

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -240,18 +240,25 @@ const Main = () => {
     }, [dispatch]);
 
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
-            Platform.OS === 'ios'
-                ? Alert.alert(
-                    'Initial Network Connectivity Type:',
-                    connectionInfo.type
-                )
-                : ToastAndroid.show(
-                    'Initial Network Connectivity Type: ' +
-                    connectionInfo.type,
-                    ToastAndroid.LONG
-                );
-        });
+        const checkInitialConnectivity = async () => {
+            try {
+                const connectionInfo = await NetInfo.fetch();
+                Platform.OS === 'ios'
+                    ? Alert.alert(
+                        'Initial Network Connectivity Type:',
+                        connectionInfo.type
+                    )
+                    : ToastAndroid.show(
+                        'Initial Network Connectivity Type: ' +
+                        connectionInfo.type,
+                        ToastAndroid.LONG
+                    );
+            } catch (error) {
+                console.error('Error fetching initial network connectivity:', error);
+            }
+        };
+
+        checkInitialConnectivity();
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
@@ -259,8 +266,32 @@ const Main = () => {
             }
         );
 
-        return unsubscribeNetInfo;
+        return () => unsubscribeNetInfo();
     }, []);
+
+    // using promise.then syntax...
+    // useEffect(() => {
+    //    NetInfo.fetch().then((connectionInfo) => {
+    //         Platform.OS === 'ios'
+    //             ? Alert.alert(
+    //                 'Initial Network Connectivity Type:',
+    //                 connectionInfo.type
+    //             )
+    //             : ToastAndroid.show(
+    //                 'Initial Network Connectivity Type: ' +
+    //                 connectionInfo.type,
+    //                 ToastAndroid.LONG
+    //             );
+    //     });
+
+    //     const unsubscribeNetInfo = NetInfo.addEventListener(
+    //         (connectionInfo) => {
+    //             handleConnectivityChange(connectionInfo);
+    //         }
+    //     );
+
+    //     return unsubscribeNetInfo;
+    // }, []);
 
     const handleConnectivityChange = (connectionInfo) => {
         let connectionMsg = 'You are now connected to an active network.';
